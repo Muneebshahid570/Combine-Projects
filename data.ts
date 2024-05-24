98c9c50066288d4b6de3159c2f8fc0ff59d598aa
Muneebshahid570 <168467556+Muneebshahid570@users.noreply.github.com>
2024-05-24 12:18:08 -0700

    Add files via upload

---
+++ data.ts
@@ -0,0 +1,46 @@
+// string data type;
+console.log('abc');
+console.log("def");
+console.log('ghi');
+
+// Number data type:
+console.log(1000);
+console.log(2000);
+console.log(5000);
+
+// boolean data type;
+console.log(true);
+console.log(false);
+console.log(2===2);
+
+// to check any data type method: "typeof"
+console.log(typeof "abc");
+console.log(typeof 5000);
+
+// variable 
+// variable declaration: let, var const;
+let box1 = "cylinder";
+box1 = "connecting rod";
+var box2 = "piston";
+const box3 = 100;
+console.log(box1);
+console.log(box2);
+console.log(box3);
+console.log(typeof box1);
+console.log(typeof box3);
+
+// block condition:
+let x = 10;
+if (x===10){
+    let mybox = 'iphone';
+    const mybox2 = 'oppo';
+    var mybox3 = 'huawei';
+    console.log(mybox);
+    console.log(mybox3);
+
+}
+
+// template literals:
+let myname = 'Muneeb Shahid';
+console.log('Mr $(myname) is a good person');
+
